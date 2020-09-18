db2246201651f5e10833559d0cf4c318ee0fad38
Mahesh Patel <m.mahesh.p.com>
2020-09-18 19:33:51 +0530

    admin changes

--- admin/controller/setting/setting.php
+++ admin/controller/setting/setting.php
@@ -879,6 +879,8 @@ class ControllerSettingSetting extends Controller {
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
+		$data['group_id'] = $this->user->getGroupId();
+	
 
 		$this->response->setOutput($this->load->view('setting/setting', $data));
 	}
